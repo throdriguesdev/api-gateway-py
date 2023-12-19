cd4d9b5828e3caa81e0ec21bdedc1378e9c45d14
Thiago <thdasrodrigues@gmail.com>
2023-12-19 15:10:17 -0300

    Lista funcionando e sendo carregada

--- projeto-um/index.js
+++ projeto-um/index.js
@@ -3,47 +3,21 @@ const fs = require('fs').promises;
 const app = express();
 const port = 3000;
 
-let emails = {};
-
-async function loadEmails() {
-    try {
-      const data = await fs.readFile('emails.json', 'utf-8');
-  
-      if (data.trim() === '') {
-        console.error('Error loading emails: File is empty');
-        return;
-      }
-  
-      console.log('Data read from emails.json:', data);
-  
-      emails = JSON.parse(data);
-    } catch (error) {
-      if (error instanceof SyntaxError) {
-        if (error.message.includes('Unexpected end of JSON input')) {
-          console.error('Error loading emails: JSON file is incomplete or empty');
-        } else {
-          console.error('Error loading emails: Invalid JSON syntax');
-        }
-      } else {
-        console.error('Error loading emails:', error.message);
-      }
-    }
-  }
-  
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Rota para obter todos os e-mails
-app.get('/emails', (req, res) => {
-  res.json(emails);
+app.get('/emails', async (req, res) => {
+  try {
+    const data = await fs.readFile('emails.json', 'utf-8');
+    const emails = JSON.parse(data);
+    res.json(emails);
+  } catch (error) {
+    console.error('Error loading emails:', error);
+    res.status(500).send('Error loading emails');
+  }
 });
 
-// Restante do código
-
-loadEmails().then(() => {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
